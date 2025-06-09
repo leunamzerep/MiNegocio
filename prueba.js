9a77d74951a96357610cb274381386b66af7942c
leunamzerep <manuelsph.315@gmail.com>
2025-06-08 21:59:07 -0500

    Add files via upload

---
+++ prueba.js
@@ -0,0 +1,5 @@
+document.getElementById('pruebaId').addEventListener('click', () => {
+    const el = document.getElementById('pruebaId');
+    el.style.color = 'black';
+    el.style.backgroundColor = 'aqua';
+});
