11909da88181cd8b24571f034acf70ff047baeca
leunamzerep <manuelsph.315@gmail.com>
2025-06-10 20:48:02 -0500

    Add BerriondaArepa page

---
+++ BerriondaArepa/berriondaArepa.js
@@ -0,0 +1,5 @@
+document.getElementById('menuButton').addEventListener('click', () => {
+    const el = document.getElementById('menuButton');
+    el.style.color = 'black';
+    el.style.backgroundColor = 'aqua';
+});
